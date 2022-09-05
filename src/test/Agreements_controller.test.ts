6ad5458917b52fe3a0db3771a949cf4deb11ca0c
mohamadaltawil350 <mohamadaltawil350@gmail.com>
2022-09-05 18:55:10 +0300

    update test agreements

--- src/test/Agreements_controller.test.ts
+++ src/test/Agreements_controller.test.ts
@@ -22,11 +22,53 @@ describe("AgreementsController", () => {
             reminder:true,
           })          
       ).not.toThrow();
-      ;
-      ;
-      
+    });
+  });
+  describe("Agreements", () => {
+
+    it("should get a Agreements correctly", async () => {
+      expect(
+        async () => await controller.getAgreements()
+      ).not.toThrow();
     });
   });
   
+  describe("Agreement", () => {
+
+    it("should get a Agreements correctly", async () => {
+      expect(
+        async () => await controller.getAgreement("63137ff3b2585dfb28df9ba6")
+      ).not.toThrow();
+    });
+  });
  
+  describe("delete", () => {
+
+    it("should delete a Agreements correctly", async () => {
+      expect(
+        async () => await controller.deleteAgreement("63137ff3b2585dfb28df9ba6")
+      ).not.toThrow();
+    });
+  });
+
+  describe("update", () => {
+
+    it("should update a Agreements correctly", async () => {
+      expect(
+        async () => await controller.updateAgreement(
+          "63137ff3b2585dfb28df9ba6",
+          {
+            name:"string",
+            parties:["fgh","hjghj"],
+            startDate:new Date(2005,1,1),
+            endDate:new Date(2005,1,1),
+            details:"this is details stvcvbcvbring",
+            attachments:[{name:"attname",url:"https://dfg.com/a.jpg",type:"String"}],
+            reminder:true,
+          }
+        )
+      ).not.toThrow();
+    });
+  });
+
 });
